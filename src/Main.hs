bcadd1400bf7e7fb09928c5f38955b4f908b9cb0
roland <rsx@bluewin.ch>
2014-04-16 21:43:00 +0200

    Main goes now to file RS4847.JPG

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,11 @@
 -- Main.hs for hexif project
 -- ----------------------------------------------------------------------------
 
-import Hexif
+import Graphics.Hexif
 
 main :: IO()
 main = do
-   exif <- fromFile "JG1111.exif"
+   -- exif <- fromFile "JG1111.exif"
+   exif <- fromFile "RS4847.exif"
    mapM_ print (allTags exif)
 
