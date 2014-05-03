6f365dab1969436e0c09266c38e5529e9dd9e5ad
roland <rsx@bluewin.ch>
2014-05-03 15:36:14 +0200

    added some comments

--- src/Main.hs
+++ src/Main.hs
@@ -8,10 +8,10 @@
 
 import Graphics.Hexif
 import System.IO
-import System.Environment
+import System.Environment (getArgs)
 import System.FilePath
 
-
+-- | main entry of the program
 main :: IO()
 main = do
      args <- getArgs
@@ -19,11 +19,13 @@ main = do
        then putStrLn "usage: ???? filename"
        else processFile $ head args
 
+-- | Pretty print all tags
 processFile :: FilePath -> IO()
 processFile fn = do
     exif <- fromFile fn
     mapM_ print (allTagsInclDebug exif)
 
+-- | Pretty print a single tag
 singleTag :: FilePath -> ExifTag -> IO()
 singleTag fn tag = do
     exif <- fromFile fn
